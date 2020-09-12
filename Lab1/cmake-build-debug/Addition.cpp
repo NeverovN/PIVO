d5713133f1d3752dc2603888f805cc05ac314ece
perf3ctstyle <56080788+perf3ctstyle@users.noreply.github.com>
2020-09-12 13:47:34 -0400

    added + and += operators
    added new operators

---
+++ Lab1/cmake-build-debug/Addition.cpp
@@ -0,0 +1,23 @@
+#include "Polynomial.h"
+
+Polynomial Polynomial::operator + (const Polynomial addendum) {
+	Polynomial result;
+	this->coefArray.size() > addendum.coefArray.size() ?
+		result.init(*this, this->coefArray.size()) :
+		result.init(*this, addendum.coefArray.size());
+	for (int i = 0; i < addendum.coefArray.size(); ++i)
+		result.coefArray[i + result.coefArray.size() - addendum.coefArray.size()] += addendum.coefArray[i];
+	result.refactor();
+	return result;
+}
+
+Polynomial Polynomial::operator += (const Polynomial addendum) {
+	this->coefArray.size() > addendum.coefArray.size() ?
+		this->init(*this, this->coefArray.size()) :
+		this->init(*this, addendum.coefArray.size());
+	for (int i = 0; i < addendum.coefArray.size(); ++i)
+		this->coefArray[i + this->coefArray.size() - addendum.coefArray.size()] =
+		this->coefArray[i + this->coefArray.size() - addendum.coefArray.size()] + addendum.coefArray[i];
+	this->refactor();
+	return *this;
+}
