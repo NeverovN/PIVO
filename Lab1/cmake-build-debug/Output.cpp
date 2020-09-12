0c8abb4cec6890de1de11c093b0d2703ce8980ea
NeverovN <neverovvnn@gmail.com>
2020-09-12 15:46:26 +0300

    debug <<

--- Lab1/cmake-build-debug/Output.cpp
+++ Lab1/cmake-build-debug/Output.cpp
@@ -9,6 +9,26 @@ using namespace std;
 
 ostream &operator<<(ostream &out, const Polynomial &output) {
     if (!output.coefArray.empty()) {
+        for (int i = 0; i < output.coefArray.size(); ++i) {
+            if (output.coefArray[i] == 0) {
+                continue;
+            }
+            if (output.coefArray[i] >= 0 && i != 0) {
+                out << "+";
+            }
+            out << output.coefArray[i];
+            if (i < output.coefArray.size() - 1) {
+                out << "x";
+                if (i < output.coefArray.size() - 2) {
+                    out << "^" << output.coefArray.size() - 1 - i;
+                }
+            }
+        }
+    }
+}
+
+/*ostream &operator<<(ostream &out, const Polynomial &output) {
+    if (!output.coefArray.empty()) {
         if (output.coefArray.size() > 2) {
             if (output.coefArray[0] != 1)
                 cout << output.coefArray[0];
@@ -28,7 +48,7 @@ ostream &operator<<(ostream &out, const Polynomial &output) {
                     cout << output.coefArray[i] << "x^" << output.coefArray.size() - i - 1;
 
         }
-        if (output.coefArray.size() > 2)
+        //if (output.coefArray.size() > 2)
             if (output.coefArray[output.coefArray.size() - 2] != 0)
                 if (output.coefArray[output.coefArray.size() - 2] > 0)
                     cout << "+" << output.coefArray[output.coefArray.size() - 2] << "x";
@@ -43,4 +63,4 @@ ostream &operator<<(ostream &out, const Polynomial &output) {
                     cout << output.coefArray[output.coefArray.size() - 1];
 
     }
-}
+}*/
