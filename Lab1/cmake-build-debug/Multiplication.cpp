a923cee623b053aa9b2b1f222baa4803995674a1
perf3ctstyle <56080788+perf3ctstyle@users.noreply.github.com>
2020-09-13 06:27:07 -0400

    added * and *= operators
    added new operators

---
+++ Lab1/cmake-build-debug/Multiplication.cpp
@@ -0,0 +1,23 @@
+#include "Polynomial.h"
+
+Polynomial Polynomial::operator * (const Polynomial multiplier) {
+	Polynomial result;
+	result.coefArray.resize(this->coefArray.size() + multiplier.coefArray.size() - 1);
+	for (int i = 0; i < this->coefArray.size(); i++) {
+		for (int j = 0; j < multiplier.coefArray.size(); j++) {
+			result.coefArray[i + j] += this->coefArray[i] * multiplier.coefArray[j];
+		}
+	}
+	return result;
+}
+
+Polynomial Polynomial::operator *= (const Polynomial multiplier) {
+	Polynomial result;
+	result.coefArray.resize(this->coefArray.size() + multiplier.coefArray.size() - 1);
+	for (int i = 0; i < this->coefArray.size(); i++) {
+		for (int j = 0; j < multiplier.coefArray.size(); j++) {
+			result.coefArray[i + j] += this->coefArray[i] * multiplier.coefArray[j];
+		}
+	}
+	return result;
+}
